ecd94e1978de2678eabf66ad0592f9828e9465b5
JanicaJensen <122334098+JanicaJensen@users.noreply.github.com>
2023-06-06 13:41:23 -0600

    port was changed, config was changed

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,23 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
+
+
+function getsequelize() {
+  console.log(process.env.DB_NAME || "byebyefresh_db");
+  console.log(process.env.DB_USER || "root");
+  console.log(process.env.DB_PASSWORD || "pass");
+  return sequelize;
+}
+
 const sequelize = new Sequelize(
-  process.env.DB_NAME ,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME || "byebyefresh_db",
+  process.env.DB_USER || "root",
+  process.env.DB_PASSWORD || "pass",
   {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306,
   }
 );
-module.exports = sequelize;
+
+module.exports = getsequelize();
